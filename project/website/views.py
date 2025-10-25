97398274be8ed3cce3ebbaac69fd14f0f5ca7f31
Ramenskii Kirill <kesha-1999-kirill@mail.ru>
2025-10-25 14:15:08 +0700

    Created section of services.

--- project/website/views.py
+++ project/website/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from .models import (
+    SiteSettings, Service
+)
 
 def index(request):
+    """Главная страница"""
+    # Получаем настройки сайта
+    try:
+        settings = SiteSettings.objects.first()
+    except:
+        settings = None
+    
+    services = Service.objects.filter(is_active=True).order_by('order')
+
     context = {
-        'products': ['Товар 1', 'Товар 2', 'Товар 3'],
+        'settings': settings,
+        'services': services,
     }
     return render(request, 'website/index.html', context)
