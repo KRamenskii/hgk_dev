21bbe0e069e21b8d1091a104abc27de82519fa63
Ramenskii Kirill <kesha-1999-kirill@mail.ru>
2025-10-28 10:06:20 +0700

    The Hero section has been created.

--- project/website/views.py
+++ project/website/views.py
@@ -17,11 +17,19 @@ def index(request):
     coverage_stats = CoverageStat.objects.filter(is_active=True).order_by('order')
     delivery_points = DeliveryPoint.objects.filter(active=True)
 
+    hero_stats = {
+        'years': '15+',
+        'clients': '1000+',
+        'support': '24/7',
+        'quality': '100%'
+    }
+
     context = {
         'settings': settings,
         'services': services,
         'advantages': advantages,
         'coverage_stats': coverage_stats,
+        'hero_stats': hero_stats,
         'delivery_points': list(delivery_points.values("name", "description", "latitude", "longitude")),
         'YANDEX_MAP_API_KEY': django_settings.YANDEX_MAP_API_KEY,
     }
