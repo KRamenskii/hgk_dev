c01bb3aa956e5277335aaf6bf6736786ecb021ae
Ramenskii Kirill <kesha-1999-kirill@mail.ru>
2025-10-30 12:59:47 +0700

    Added cookies and privacy policy.

--- project/website/static/js/main.js
+++ project/website/static/js/main.js
@@ -219,3 +219,14 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (!localStorage.getItem("cookiesAccepted")) {
+        document.getElementById("cookie-banner").style.display = "block";
+    }
+});
+
+function acceptCookies() {
+    localStorage.setItem("cookiesAccepted", "true");
+    document.getElementById("cookie-banner").style.display = "none";
+}
--- project/website/urls.py
+++ project/website/urls.py
@@ -1,6 +1,8 @@
+from django.views.generic import TemplateView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('privacy/', TemplateView.as_view(template_name='privacy.html'), name='privacy'),
 ]
