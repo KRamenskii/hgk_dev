1b42b4485693fe9e445fbf01d640246116f6cab3
Ramenskii Kirill <kesha-1999-kirill@mail.ru>
2025-11-05 16:17:44 +0700

    test deploy

--- project/project/settings/prod.py
+++ project/project/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['xn--19-glc2a5c.xn--p1ai.com', 'www.xn--19-glc2a5c.xn--p1ai'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['xn--19-glc2a5c.xn--p1ai', 'www.xn--19-glc2a5c.xn--p1ai'])
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
