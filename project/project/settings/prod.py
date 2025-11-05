40072174ac6bfade1b32a872af8753b474fdd91e
Ramenskii Kirill <kesha-1999-kirill@mail.ru>
2025-11-05 19:46:34 +0700

    test deploy

--- project/project/settings/prod.py
+++ project/project/settings/prod.py
@@ -13,11 +13,11 @@ CSRF_TRUSTED_ORIGINS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('DB_HOST', default='db'),
+        'PORT': env('DB_PORT', default=5432),
     }
 }
 
